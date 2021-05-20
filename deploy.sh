3b62801b85a649c16cbbe9a8aac5814015a5fccf
Ariel <ariel@craftech.io>
2021-05-20 20:24:57 -0300

    chore: remove dependency update

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,6 @@ else
         update-kubeconfig --name ${CLUSTER_NAME} 
 fi
 
-# Helm Dependency Update
-helm dependency update ${DEPLOY_CHART_PATH:-helm/}
-
 # Helm Deployment
 UPGRADE_COMMAND="helm upgrade --wait --atomic --install --timeout ${TIMEOUT}"
 for config_file in ${DEPLOY_CONFIG_FILES//,/ }
@@ -27,6 +24,6 @@ fi
 if [ -n "$DEPLOY_VALUES" ]; then
     UPGRADE_COMMAND="${UPGRADE_COMMAND} --set ${DEPLOY_VALUES}"
 fi
-UPGRADE_COMMAND="${UPGRADE_COMMAND} ${DEPLOY_NAME} ${DEPLOY_CHART_PATH:-helm/}"
+UPGRADE_COMMAND="${UPGRADE_COMMAND} ${DEPLOY_NAME} ${DEPLOY_CHART_PATH}"
 echo "Executing: ${UPGRADE_COMMAND}"
 ${UPGRADE_COMMAND}
