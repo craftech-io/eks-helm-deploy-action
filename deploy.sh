75830bae61c5ebc9c70f6d706d103edb6c950f68
Ariel <ariel@craftech.io>
2021-05-20 20:01:30 -0300

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Login to Kubernetes Cluster.
+if [ -n "$CLUSTER_ROLE_ARN" ]; then
+    aws eks \
+        --region ${AWS_REGION} \
+        update-kubeconfig --name ${CLUSTER_NAME} \
+        --role-arn=${CLUSTER_ROLE_ARN}
+else
+    aws eks \
+        --region ${AWS_REGION} \
+        update-kubeconfig --name ${CLUSTER_NAME} 
+fi
+
+# Helm Dependency Update
+helm dependency update ${DEPLOY_CHART_PATH:-helm/}
+
+# Helm Deployment
+UPGRADE_COMMAND="helm upgrade --wait --atomic --install --timeout ${TIMEOUT}"
+for config_file in ${DEPLOY_CONFIG_FILES//,/ }
+do
+    UPGRADE_COMMAND="${UPGRADE_COMMAND} -f ${config_file}"
+done
+if [ -n "$DEPLOY_NAMESPACE" ]; then
+    UPGRADE_COMMAND="${UPGRADE_COMMAND} -n ${DEPLOY_NAMESPACE}"
+fi
+if [ -n "$DEPLOY_VALUES" ]; then
+    UPGRADE_COMMAND="${UPGRADE_COMMAND} --set ${DEPLOY_VALUES}"
+fi
+UPGRADE_COMMAND="${UPGRADE_COMMAND} ${DEPLOY_NAME} ${DEPLOY_CHART_PATH:-helm/}"
+echo "Executing: ${UPGRADE_COMMAND}"
+${UPGRADE_COMMAND}
