49045230bd8f2f87b9bbcb0c8cb8daa5964f7eb4
Blanca Ayala <ayalaluquez@riseup.net>
2021-06-09 10:42:54 -0300

    fix: remove wait, atomic and install option

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ else
 fi
 
 # Helm Deployment
-UPGRADE_COMMAND="helm upgrade --wait --atomic --install --timeout ${TIMEOUT}"
+UPGRADE_COMMAND="helm upgrade --timeout ${TIMEOUT}"
 for config_file in ${DEPLOY_CONFIG_FILES//,/ }
 do
     UPGRADE_COMMAND="${UPGRADE_COMMAND} -f ${config_file}"
