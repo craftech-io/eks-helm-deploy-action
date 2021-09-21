13b14932bbc55949014dfa10dd3f5a803b955dee
Ariel <ariel@craftech.io>
2021-09-20 21:33:50 -0300

    test: separate update from upgrade

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,8 @@ else
 fi
 
 # Helm Deployment
-DEPS_UPDATE_COMMAND=""
-UPGRADE_COMMAND="helm dependency update && helm upgrade --timeout ${TIMEOUT}"
+DEPS_UPDATE_COMMAND="helm dependency update"
+UPGRADE_COMMAND="helm upgrade --timeout ${TIMEOUT}"
 for config_file in ${DEPLOY_CONFIG_FILES//,/ }
 do
     UPGRADE_COMMAND="${UPGRADE_COMMAND} -f ${config_file}"
@@ -25,10 +25,8 @@ fi
 if [ -n "$DEPLOY_VALUES" ]; then
     UPGRADE_COMMAND="${UPGRADE_COMMAND} --set ${DEPLOY_VALUES}"
 fi
-if [ "$UPDATE_DEPS" = true ]; then
-    UPGRADE_COMMAND="${DEPS_UPGRADE_COMMAND} ${UPGRADE_COMMAND} --set ${DEPLOY_VALUES}"
-fi
 UPGRADE_COMMAND="${UPGRADE_COMMAND} ${DEPLOY_NAME} ${DEPLOY_CHART_PATH}"
-echo "${UPDATE_DEPS}"
+echo "Executing: ${DEPS_UPDATE_COMMAND}"
+${DEPS_UPDATE_COMMAND}
 echo "Executing: ${UPGRADE_COMMAND}"
 ${UPGRADE_COMMAND}
